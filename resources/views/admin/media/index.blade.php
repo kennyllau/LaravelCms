1cce89dee2dbb1a4d61e3f672d78b9b77cfc9fd7
kenny lau <kennyllau9@gmail.com>
2016-10-08 17:08:06 -0700

    Delete Button for Images in Media Index working

--- resources/views/admin/media/index.blade.php
+++ resources/views/admin/media/index.blade.php
@@ -6,12 +6,13 @@
 	<h1>Media</h1>
 
 	@if($photos)
-		<table class="table table-bordered table-hover">
+		<table class="table table-hover">
 			<thead>
 				<tr>
 					<th>Id</th>
 					<th>Name</th>
 					<th>Created</th>
+					<th>Action</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -22,7 +23,17 @@
 						<td><img height="50" src="{{$photo->file}}" alt=""></td>
 
 						<td>{{$photo->created_at ? $photo->created_at : 'no date'}}</td>
-						
+
+						<td>
+							{!! Form::open(['method'=>'DELETE', 'action'=>['AdminMediasController@destroy', $photo->id]])!!}
+
+								<div class="form-group">
+									{!! Form::submit('Delete', ['class'=>'btn btn-danger']) !!}
+								</div>
+
+							{!! Form::close() !!}
+						</td>
+
 					</tr>
 				@endforeach
 			</tbody>
